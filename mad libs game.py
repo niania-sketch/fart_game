71230623022a8b94f106280f006bca56320d08b2
niania-sketch <139045168+niania-sketch@users.noreply.github.com>
2024-02-02 18:28:58 +0400

    Add files via upload

---
+++ mad libs game.py
@@ -0,0 +1,18 @@
+
+
+
+musical_instrument = input("enter a musical instrument: ")
+animal_sound = input("enter an animal sound: ")
+flavor = input("enter a flavor: ")
+vegetable = input("enter a vegetable: ")
+fast_food = input("enter a fast food: ")
+verb1 = input("enter a verb: ")
+dessert = input("enter a dessert: ")
+adjective = input("enter an adjective: ")
+verb2 = input("enter a verb: ")
+
+print("everyone farts! It's a fact! Some people\nhave farts that sound like " + musical_instrument + ",""\nothers sound like " + animal_sound + ".""Some\n farts smell like " + flavor + "," "others like\n " + vegetable + "." "If you eat too many " + fast_food + "," "your farts may " + verb1 + "right out of you\nsmell like rotted " + dessert + "." "Some farts are silent but " + adjective + "." "whatever kind of fart you " + verb2 + " out of yourself, be sure to \nsay 'Excuse me!'")
+
+
+
+
